a28f41fc38364dc629c78425fd246923fbe2c650
xuld <xuld@vip.qq.com>
2012-08-16 17:07:32 +0800

    合成

--- controls/button/assets/scripts/linkbutton.js
+++ controls/button/assets/scripts/linkbutton.js
@@ -1,1 +1,1 @@
-/** * @author  */
+imports("Controls.Button.LinkButton");var LinkButton = Control.extend({		tpl: '<a class="x-linkbutton" href="javascript://" target="_blank"></a>',		setHref: function(value){		return this.setAttr('href', value);	},		getHref: function(){		return this.getAttr('href');	}	});
