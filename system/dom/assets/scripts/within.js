f5847da8dd773cc249c137d7d68d0b5224669299
xuld <xuld@vip.qq.com>
2012-08-17 10:23:30 +0800

    within

--- system/dom/assets/scripts/within.js
+++ system/dom/assets/scripts/within.js
@@ -5,10 +5,23 @@
 
 Dom.implement({
 	
-	within: function (bound) {
-		return ((bound.right < box.right && bound.right > box.left) || (bound.left < box.right && bound.left > box.left)) &&
-			  ((bound.bottom < box.bottom && bound.bottom > box.top) || (bound.top < box.bottom && bound.top > box.top)) ;
+	within: function (x,y,width,height) {
+		var container,
+			containerPos,
+			containerSize,
+			targetPos = this.getPosition(),
+			targetSize = targetPos.add(this.getSize());
+		if(typeof(x)=='String'){
+			container = Dom.get(id);
+			containerPos = container.getPosition();
+			containerSize = containerPos.add(container.getSize());
+			targetPos = this.getPosition();
+			targetSize = targetPos.add(this.getSize());
+		}else{
+			containerPos = {x:x,y:y};
+			containerSize = {x:containerPos.x+(width||0),y:containerPos.y+(height||0)};			
+		}
+		return Math.max(targetPos.x, containerPos.x) <= Math.min(targetSize.x, containerSize.x) &&
+			Math.max(targetPos.y, containerPos.y) <= Math.min(targetSize.y, containerSize.y);
 	}
-
-
 });
