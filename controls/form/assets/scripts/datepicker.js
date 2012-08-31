4a43a6a430bc96526b2f06098f952dd01e933958
xuld <xuld@vip.qq.com>
2012-08-31 19:47:30 +0800

    数据选择器

--- controls/form/assets/scripts/datepicker.js
+++ controls/form/assets/scripts/datepicker.js
@@ -1,5 +1,5 @@
-/** * @author  */imports("Controls.Display.Icon");using("Controls.Core.IMenuContainer");using("Controls.Form.CombinedTextBox");using("Controls.Form.MonthCalender");var DatePicker = Picker.implement({		menuWidth: -1,		menuButtonTpl: '<button class="x-button"><span class="x-icon x-icon-calendar"></span></button>',		init: function (options) {		this.base('init');		this.setMenu(new MonthCalender().on('itemclick', this.onItemClick, this));		this.button().on('click', this.toggleMenu, this);		this.on('focus', this.showMenu);	}, 		format: 'yyyy/M/d',		onSelect: function (value) {		this.setValue(value);				this.hideMenu();	},		// updateMenu: function(){
-		// var d = new Date(this.getText());
-		// if(!isNaN(d.getYear()))
-			// this.menu.setValue(d);
-	// },		getValue: function(){		return this.menu.getValue();	},		setValue: function(value){		this.setText(value.toString(this.format));		this.menu.setValue(value);		return this;	}});
+/** * @author  */imports("Controls.Display.Icon");using("Controls.Form.Picker");using("Controls.Form.MonthCalender");var DatePicker = Picker.extend({		format: 'yyyy/M/d',		dropDownWidth: -1,		dropDownButtonTpl: '<button class="x-button"><span class="x-icon x-icon-calendar"></span></button>',		createDropDown: function(existDom){		return new MonthCalender(existDom).on('select', this.selectItem, this);	},		init: function (options) {		Picker.prototype.init.call(this, options);		this.input().on('focus', this.showDropDown, this);		this.input().on('blur', this.hideDropDown, this);	}, 		selectItem: function (item) {		this.setValue(this.dropDown.getValue());		this.hideDropDown();	},		updateDropDown: function(){
+		var d = new Date(this.getText());
+		if(!isNaN(d.getYear()))
+			this.dropDown.setValue(d);
+	},		getValue: function(){		return this.dropDown.getValue();	},		setValue: function(value){		this.setText(value.toString(this.format));		this.dropDown.setValue(value);		return this;	}});
