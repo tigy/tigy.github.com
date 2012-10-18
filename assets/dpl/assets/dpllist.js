2cb3f56416a060aa0dee58a5bda4f8d6183180bb
xuld <xuld@vip.qq.com>
2012-10-18 09:31:01 +0800

    瀑布流

--- assets/dpl/assets/dpllist.js
+++ assets/dpl/assets/dpllist.js
@@ -76,10 +76,10 @@ Demo.writeDplList = function (values) {
     Demo.DplList.list = DplList;
     Demo.DplList.tree = Demo.listToTree(DplList);
 
-    var list = Demo.DplList.list, tree = Demo.DplList.tree, key, a, ai, b, bi, c, controlInfo, html = '', html2, all, finish;
-
-    var from = decodeURIComponent((/(&|\?)from=(.*?)(&|$)/.exec(location.href) || [0, 0, ""])[2]);
-
+    var list = Demo.DplList.list, tree = Demo.DplList.tree, key, a, ai, b, bi, c, controlInfo, html = '', html2, all, finish,
+    	from = decodeURIComponent((/(&|\?)from=(.*?)(&|$)/.exec(location.href) || [0, 0, ""])[2]),
+    	column = 5;
+    
     for (key in values) {
 
         finish = all = 0;
@@ -101,7 +101,7 @@ Demo.writeDplList = function (values) {
 
                 var url = Demo.getDemoUrl(c);
 
-                html2 += '<li><a href="' + url + '" class="demo-mono status-' + controlInfo.status + (from === url ? ' current' : '') + '">' + bi + '(' + controlInfo.name + ')</a></li>';
+                html2 += '<li><a href="' + url + '" class="demo-mono status-' + controlInfo.status + (from === url ? ' current' : '') + '" title="状态: ' + (Demo.Configs.status[controlInfo.status] || '已完成') + '">' + bi + '(' + controlInfo.name + ')</a></li>';
 
                 switch (controlInfo.status) {
                     case 'ok':
@@ -123,11 +123,63 @@ Demo.writeDplList = function (values) {
         }
 
         if (html2) {
-            html += '<article class="demo demo-grid demo-grid-5"><nav class="demo-toolbar"><a href="javascript://在此分类添加一个组件" class="x-linkbutton" onclick="Demo.DplList.addDpl(this.parentNode, \'' + key + '\')">✚ 添加组件</a></nav><h2 class="demo">' + key + '(' + values[key] + ')' + '<small>' + finish + '/' + all + '</small></h2>' + html2 + '</article>';
+            html += '<article class="demo demo-relative demo-grid demo-grid-' + column + '"><nav class="demo-toolbar"><a href="javascript://在' + key + '下添加一个组件" title="在' + key + '下添加一个组件" class="x-linkbutton" onclick="Demo.DplList.addDpl(this.parentNode, \'' + key + '\')">✚ 添加组件</a></nav><h2 class="demo">' + key + '(' + values[key] + ')' + '<small>' + finish + '/' + all + '</small></h2>' + html2 + '</article>';
         }
 
     }
 
     document.write(html);
+    
+    // 转为瀑布流。
+    
+    var articles = document.getElementsByTagName("ARTICLE"),
+    	articlesIndex,
+    	sections,
+    	len,
+    	heights = [],
+    	section,
+    	min,
+    	minIndex,
+    	baseHeight = 40,
+    	marginRight = 10,
+    	marginBottom = 30,
+    	columnWidth,
+    	i,
+    	j;
+    
+    // 每个 ARTICLE 分开处理。
+    for(articlesIndex = 0; articles[articlesIndex]; articlesIndex++){
+    	if(articles[articlesIndex].className.indexOf("demo-grid") >= 0) {
+	    	sections = articles[articlesIndex].getElementsByTagName("SECTION");
+	    	len = sections.length;
+	    	heights.length = 0;
+	    	columnWidth = len && sections[0].offsetWidth + marginRight;
+	    	
+		    // 第一行不处理。
+		    for(i = 0; i < column; i++) {
+		    	if(section = sections[i]){
+		    		heights[i] = baseHeight + section.offsetHeight + marginBottom;
+		    	}
+		    }
+		    
+		    // 每个块依次选择合适的位置。
+		    for(i = column; i < len; i++) {
+		    	min = Infinity;
+		    	for(j = heights.length; --j >=0;){
+		    		if(min > heights[j]){
+		    			minIndex = j;
+		    			min = heights[j];
+		    		}
+		    	}
+		    	
+		    	section = sections[i];
+		    	section.style.position = 'absolute';
+		    	section.style.top = min + 'px';
+		    	section.style.left = minIndex * columnWidth + 'px';
+		    	
+		    	heights[minIndex] += section.offsetHeight + marginBottom;
+		    }
+	    }
+    }
 
 };
