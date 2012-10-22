61dbf503353e5494a796ea040ec192599847128c
xuld <xuld@vip.qq.com>
2012-10-22 13:03:57 +0800

    常用组件

--- src/Controls/Form/assets/scripts/TextBox.js
+++
@@ -1,1 +0,0 @@
-/** * @author  xuld */imports("Controls.Form.TextBox");using("Controls.Core.IInput");var TextBox = Control.extend(IInput).implement({		xtype: 'textbox',		tpl: '<input type="text" class="x-control">'	});
