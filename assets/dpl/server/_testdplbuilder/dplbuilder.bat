4d58fc37c761e3d28942a9657537974c6700a673
xuld <xuld@vip.qq.com>
2012-11-01 17:47:59 +0800

    工具

---
+++ assets/dpl/server/_testdplbuilder/dplbuilder.bat
@@ -0,0 +1,5 @@
+@cd ../../
+:start
+@"assets/tools/node/win/node.exe" assets/bin/test/dplbuilder.js
+@pause
+@goto start
---
+++ resources/unittest/unit/base.js
@@ -0,0 +1,349 @@
+module("Base");
+
+test("基本的坏境需求", function () {
+	expect(6);
+	ok(Array.prototype.push, "Array.push()");
+	ok(Function.prototype.apply, "Function.apply()");
+	ok(document.getElementById, "getElementById");
+	ok(document.getElementsByTagName, "getElementsByTagName");
+	ok(RegExp, "RegExp");
+	ok(JPlus, "System");
+});
+
+test( "execScript", function() {
+
+	expect( 3 );
+
+	execScript( "var globalEvalTest = true;" );
+	ok( window.globalEvalTest, "Test variable declarations are global" );
+
+	window.globalEvalTest = false;
+
+	execScript( "globalEvalTest = true;" );
+	ok( window.globalEvalTest, "Test variable assignments are global" );
+
+	window.globalEvalTest = false;
+
+	execScript( "this.globalEvalTest = true;" );
+	ok( window.globalEvalTest, "Test context (this) is the window object" );
+
+	window.globalEvalTest = undefined;
+});
+
+// test("Object.type", function() {
+// 
+	// equal( Object.type(null), "null", "null" );
+	// equal( Object.type(undefined), "undefined", "undefined" );
+	// equal( Object.type(true), "boolean", "Boolean" );
+	// equal( Object.type(false), "boolean", "Boolean" );
+	// equal( Object.type(Boolean(true)), "boolean", "Boolean" );
+	// equal( Object.type(0), "number", "Number" );
+	// equal( Object.type(1), "number", "Number" );
+	// equal( Object.type(Number(1)), "number", "Number" );
+	// equal( Object.type(""), "string", "String" );
+	// equal( Object.type("a"), "string", "String" );
+	// equal( Object.type(String("a")), "string", "String" );
+	// equal( Object.type({}), "object", "Object" );
+	// equal( Object.type(/foo/), "regexp", "RegExp" );
+	// equal( Object.type(new RegExp("asdf")), "regexp", "RegExp" );
+	// equal( Object.type([1]), "array", "Array" );
+	// equal( Object.type(new Date()), "date", "Date" );
+	// equal( Object.type(new Function("return;")), "function", "Function" );
+	// equal( Object.type(function(){}), "function", "Function" );
+	// equal( Object.type(window), "object", "Window" );
+	// equal( Object.type(document), "control", "Document" );
+	// equal( Object.type(Dom.get(document.body)), "control", "Element" );
+	// equal( Object.type(document.createTextNode("foo")), "object", "TextNode" );
+// 	
+	// // !Safari
+	// //equal( Object.type(document.getElementsByTagName("*")), "object", "DomList" );
+// });
+
+test("Object.extend", function() {
+
+	var target = { toString: 1 },
+		expected ={ toString: 2};
+
+	Object.extend(target, expected);
+	deepEqual( target, expected, "内置成员" );
+
+});
+
+test("Object.each", function() {
+	expect(13);
+	Object.each( [0,1,2], function(n, i){
+		equal( i, n, "Check array iteration" );
+	});
+
+	Object.each( [5,6,7], function(n, i){
+		equal( i, n - 5, "Check array iteration" );
+	});
+
+	Object.each( { name: "name", lang: "lang" }, function(n, i){
+		equal( i, n, "Check object iteration" );
+	});
+
+	var total = 0;
+	Object.each([1,2,3], function(v, i){ total += v; });
+	equal( total, 6, "Looping over an array" );
+	total = 0;
+	Object.each([1,2,3], function(v, i){ total += v; if ( i == 1 ) return false; });
+	equal( total, 3, "Looping over an array, with break" );
+	total = 0;
+	Object.each({"a":1,"b":2,"c":3}, function(v, i){ total += v; });
+	equal( total, 6, "Looping over an object" );
+	total = 0;
+	Object.each({"a":3,"b":3,"c":3}, function(v, i){ total += v; return false; });
+	equal( total, 3, "Looping over an object, with break" );
+
+	//var f = function(){};
+	//f.foo = "bar";
+	//Object.each(f, function(v, i){
+	//	f[i] = "baz";
+	//});
+	//equal( "baz", f.foo, "Loop over a function" );
+
+	var stylesheet_count = 0;
+	Object.each(document.styleSheets, function(i){
+		stylesheet_count++;
+	});
+	ok(stylesheet_count, "should not throw an error in IE while looping over document.styleSheets and return proper amount");
+
+});
+
+test("Object.isArray", function() {
+	expect(17);
+
+	// Make sure that false values return false
+	ok( !Object.isArray(), "No Value" );
+	ok( !Object.isArray( null ), "null Value" );
+	ok( !Object.isArray( undefined ), "undefined Value" );
+	ok( !Object.isArray( "" ), "Empty String Value" );
+	ok( !Object.isArray( 0 ), "0 Value" );
+
+	// Check built-ins
+	// Safari uses "(Internal Function)"
+	ok( !Object.isArray(String), "String Function("+String+")" );
+	ok( !Object.isArray(Array), "Array Function("+Array+")" );
+	ok( !Object.isArray(Object), "Object Function("+Object+")" );
+	ok( !Object.isArray(Function), "Function Function("+Function+")" );
+
+	// When stringified, this could be misinterpreted
+	var mystr = "function";
+	ok( !Object.isArray(mystr), "Function String" );
+
+	// When stringified, this could be misinterpreted
+	var myarr = [ "function" ];
+	ok( Object.isArray(myarr), "Function Array" );
+
+	// When stringified, this could be misinterpreted
+	var myArray = { "function": "test", length: 3 };
+	ok( !Object.isArray(myArray), "Function Object" );
+
+	// Make sure normal functions still work
+	var fn = function(){};
+	ok( !Object.isArray(fn), "Normal Function" );
+
+	var obj = document.createElement("object");
+
+	// Firefox says this is a function
+	ok( !Object.isArray(obj), "Object Element" );
+
+	// IE says this is an object
+	// Since 1.3, this isn't supported (#2968)
+	//ok( Object.isArray(obj.getAttribute), "getAttribute Function" );
+
+	var nodes = document.body.childNodes;
+
+	ok( !Object.isArray(nodes), "childNodes Property" );
+
+	var first = document.body.firstChild;
+
+	// Normal elements are reported ok everywhere
+	ok( !Object.isArray(first), "A normal DOM Element" );
+
+	var input = document.createElement("input");
+	input.type = "text";
+	document.body.appendChild( input );
+
+	// IE says this is an object
+	// Since 1.3, this isn't supported (#2968)
+	//ok( Object.isArray(input.focus), "A default function property" );
+
+	document.body.removeChild( input );
+
+	var a = document.createElement("a");
+	a.href = "some-function";
+	document.body.appendChild( a );
+
+	// This serializes with the word 'function' in it
+	ok( !Object.isArray(a), "Anchor Element" );
+
+	document.body.removeChild( a );
+});
+
+test("Array.create", function(){
+
+	// equal( Array.create(document.findAll("head")[0]s)[0].nodeName.toUpperCase(), "HEAD", "Pass makeArray a List object" );
+
+	// equal( Array.create(document.getElementsByName("PWD")).slice(0,1)[0].name, "PWD", "Pass makeArray a nodelist" );
+
+	equal( (function(){ return Array.create(arguments); })(1,2).join(""), "12", "Pass makeArray an arguments array" );
+
+	equal( Array.create([1,2,3]).join(""), "123", "Pass makeArray a real array" );
+
+	equal( Array.create().length, 0, "Pass nothing to makeArray and expect an empty array" );
+
+	equal( Array.create( [0] )[0], 0 , "Pass makeArray a number" );
+	
+	equal( Array.create( {length:2, 0:"a", 1:"b"} ).join(""), "ab", "Pass makeArray an array like map (with length)" );
+
+	// ok( !!Array.create( document.documentElement.childNodes ).slice(0,1)[0].nodeName, "Pass makeArray a childNodes array" );
+
+	// ok( Array.create(document.getElementById("form")).length >= 13, "Pass makeArray a form (treat as elements)" );
+
+	deepEqual( Array.create({length: "0"}), [], "Make sure object is coerced properly.");
+});
+
+test("Function.prototype.bind", function(){
+
+	var test = function(){ equal( this, thisObject, "Make sure that scope is set properly." ); };
+	var thisObject = { foo: "bar", method: test };
+
+	// Make sure normal works
+	test.call( thisObject );
+
+	// Basic scoping
+	test.bind( thisObject )();
+});
+
+test("Object.isFunction", function() {
+	expect(19);
+
+	// Make sure that false values return false
+	ok( !Object.isFunction(), "No Value" );
+	ok( !Object.isFunction( null ), "null Value" );
+	ok( !Object.isFunction( undefined ), "undefined Value" );
+	ok( !Object.isFunction( "" ), "Empty String Value" );
+	ok( !Object.isFunction( 0 ), "0 Value" );
+
+	// Check built-ins
+	// Safari uses "(Internal Function)"
+	ok( Object.isFunction(String), "String Function("+String+")" );
+	ok( Object.isFunction(Array), "Array Function("+Array+")" );
+	ok( Object.isFunction(Object), "Object Function("+Object+")" );
+	ok( Object.isFunction(Function), "Function Function("+Function+")" );
+
+	// When stringified, this could be misinterpreted
+	var mystr = "function";
+	ok( !Object.isFunction(mystr), "Function String" );
+
+	// When stringified, this could be misinterpreted
+	var myarr = [ "function" ];
+	ok( !Object.isFunction(myarr), "Function Array" );
+
+	// When stringified, this could be misinterpreted
+	var myfunction = { "function": "test" };
+	ok( !Object.isFunction(myfunction), "Function Object" );
+
+	// Make sure normal functions still work
+	var fn = function(){};
+	ok( Object.isFunction(fn), "Normal Function" );
+
+	var obj = document.createElement("object");
+
+	// Firefox says this is a function
+	ok( !Object.isFunction(obj), "Object Element" );
+
+	// IE says this is an object
+	// Since 1.3, this isn't supported (#2968)
+	//ok( Object.isFunction(obj.getAttribute), "getAttribute Function" );
+
+	var nodes = document.body.childNodes;
+
+	// Safari says this is a function
+	ok( !Object.isFunction(nodes), "childNodes Property" );
+
+	var first = document.body.firstChild;
+
+	// Normal elements are reported ok everywhere
+	ok( !Object.isFunction(first), "A normal DOM Element" );
+
+	var input = document.createElement("input");
+	input.type = "text";
+	document.body.appendChild( input );
+
+	// IE says this is an object
+	// Since 1.3, this isn't supported (#2968)
+	//ok( Object.isFunction(input.focus), "A default function property" );
+
+	document.body.removeChild( input );
+
+	var a = document.createElement("a");
+	a.href = "some-function";
+	document.body.appendChild( a );
+
+	// This serializes with the word 'function' in it
+	ok( !Object.isFunction(a), "Anchor Element" );
+
+	document.body.removeChild( a );
+
+	// Recursive function calls have lengths and array-like properties
+	function callme(callback){
+		function fn(response){
+			callback(response);
+		}
+
+		ok( Object.isFunction(fn), "Recursive Function Call" );
+
+		fn({ some: "data" });
+	};
+
+	callme(function(){
+		callme(function(){});
+	});
+});
+
+test("String.format", function() {
+
+	var nbsp = String.fromCharCode(160);
+
+	equal( String.format("{0} {1}  {2}", 1, 2 ,3 ), "1 2  3", "格式化{序号}" );
+	equal( String.format("{a} {b}  {c}", {a:1, b:2, c:3} ), "1 2  3", "格式化{字段}" );
+	equal( String.format("{2} {2}  {2}", 1, 2 ,3 ), "3 3  3", "重复序号" );
+	equal( String.format("{2}" ), "", "不存在的序号" );
+	equal( String.format.call(function(){return 3}, "{0}",  1 ), "3", "自定义格式化对象的函数" );
+	equal( String.format("{{}} {0}", 1), "{} 1", "格式化的字符串内有 { 和  }" );
+});
+
+test("String.prototype.trim", function() {
+
+	var nbsp = String.fromCharCode(160);
+
+	equal( "hello  ".trim(), "hello", "尾部" );
+	equal( "  hello".trim(), "hello", "头部" );
+	equal( "  hello   ".trim(), "hello", "全部" );
+	equal( ("  " + nbsp + "hello  " + nbsp + " ")    .trim(), "hello", "&nbsp;" );
+
+	equal( "".trim(), "", "空字符串" );
+});
+
+test("String.prototype.toCamelCase", function() {
+
+	var tests = {
+		"foo-bar": "fooBar",
+		"foo-bar-baz": "fooBarBaz",
+		"girl-u-want": "girlUWant",
+		"the-4th-dimension": "the4thDimension",
+		"-o-tannenbaum": "OTannenbaum",
+		"-moz-illa": "MozIlla"
+	};
+
+	expect(6);
+
+	Object.each( tests, function( val, key ) {
+		equal( key.toCamelCase(), val, "Converts: " + key + " => " + val );
+	});
+});
+
+
